360cb0235478388d626e04ccd6c162611686eecd
Nikolai <nikolai@stasj.com>
2013-12-13 19:29:24 +0100

    added jquery

--- whoamicheat.js
+++ whoamicheat.js
@@ -1,1 +1,3 @@
-(waic=function(){var e=$("#what_guesses input[name='user_id']").toArray().map(function(e){return $(e).val()});for(var t=0;t<4;t++){$.get("https://internal.isfit.org/users/"+e[t]+".html",function(e){var t=$(e).find(".user h1").text();var n=$(e).find(".picture img").attr("src");if(n===$(".picture img").attr("src")){$("input[value='"+t+"'").click()}})}})()
+$(document).ready(function() {
+   (waic=function(){var e=$("#what_guesses input[name='user_id']").toArray().map(function(e){return $(e).val()});for(var t=0;t<4;t++){$.get("https://internal.isfit.org/users/"+e[t]+".html",function(e){var t=$(e).find(".user h1").text();var n=$(e).find(".picture img").attr("src");if(n===$(".picture img").attr("src")){$("input[value='"+t+"'").click()}})}})()
+});
